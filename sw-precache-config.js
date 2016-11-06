13d406a3427698775029750a398179bbaa737108
Sławomir Momot <slawomirmomot@MacBook-Pro-Sawomir.local>
2016-11-06 14:23:09 +0100

    Iron list data and temporary data

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -11,7 +11,8 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js', 
+    '/images/untitled.svg'
   ],
   navigateFallback: '/index.html'
 };
